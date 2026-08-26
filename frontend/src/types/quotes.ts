e04c75fa26ee61374f68c53a26c01fb128ae9304
Fabrizio <epicamente@yahoo.it>
2026-08-26 16:20:37 +0200

    update citazioni

--- frontend/src/types/quotes.ts
+++ frontend/src/types/quotes.ts
@@ -1,18 +1,9 @@
 // frontend/src/types/quotes.ts
 
-export type QuoteCategory = 
-  | 'Filosofia' 
-  | 'Motivazione' 
-  | 'Saggezza' 
-  | 'Crescita' 
-  | 'Creatività' 
-  | 'Focus';
-
 export interface Quote {
   id: number;
   text: string;
   author: string;
-  category: QuoteCategory;
 }
 
 export interface QuoteCardProps {
